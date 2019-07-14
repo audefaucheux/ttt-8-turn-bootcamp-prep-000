36775e2334f4c46e1843268880a8eb8261e14a29
Aude Faucheux <aude53@gmail.com>
2019-07-14 12:41:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,8 +35,8 @@ end
 
 # turn
 def turn(board)
-input_check = true
-  until input_check == false do
+input_check = false
+  until input_check == true do
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
