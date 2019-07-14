9a2cf273d0cdc7577f468a2352aa2323ab387154
Aude Faucheux <aude53@gmail.com>
2019-07-14 12:42:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ input_check = false
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    puts input_check = valid_move?(board,index)
+    input_check = valid_move?(board,index)
   end
   move(board,index)
   display_board(board)
